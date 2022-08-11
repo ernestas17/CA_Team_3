03da6eff177b5f2289a38a2b5ad88e26479502e6
Edvinas Juska <edvinasjuska0@gmail.com>
2022-08-11 18:39:38 +0300

    Added FAQ

---
+++ src/pages/Home/FAQSection/FAQSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import CTA from "../../../components/CTA/CTA";
+import { Div, H3 } from "./FAQSection.style";
+
+const FAQSection = () => {
+  return (
+    <Div>
+      <H3>Frequently Asked Questions</H3>
+      <CTA
+        question="How long should a car repair take?"
+        anwser={
+          "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car I have got my car repaired at Finsweet I have got my car repaired at Finsweet"
+        }
+      ></CTA>
+
+      <CTA
+        question="How do I schedule my car's appointment?"
+        anwser={
+          "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car I have got my car repaired at Finsweet I have got my car repaired at Finsweet"
+        }
+      ></CTA>
+
+      <CTA
+        question="What are the repair services provided?"
+        anwser={
+          "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car I have got my car repaired at Finsweet I have got my car repaired at Finsweet"
+        }
+      ></CTA>
+
+      <CTA
+        question="How do I find auto body shops near me?"
+        anwser={
+          "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car I have got my car repaired at Finsweet I have got my car repaired at Finsweet"
+        }
+      ></CTA>
+
+      <CTA
+        question="Genuine spare parts during car repair?"
+        anwser={
+          "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car I have got my car repaired at Finsweet I have got my car repaired at Finsweet"
+        }
+      ></CTA>
+    </Div>
+  );
+};
+
+export default FAQSection;
---
+++ src/pages/Home/FAQSection/FAQSection.style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  background-color: var(--dark-gray);
+
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  padding-top: 152px;
+  padding-bottom: 128px;
+`;
+
+export const H3 = styled.h3`
+  font-size: 48px;
+  text-align: center;
+  margin-bottom: 90px;
+`;
